48e28ea556c0caa30e920e39b6bcfa29fb602307
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-06-28 00:55:05 +0500

    idk ijh

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ app.use(cors())
 app.use(express.json())
 
 /*(/api/auth)-->idhr middleware function exceute hoga ,(./ routes/auth)--> idhr middleware function hy */ 
-app.use('/api/auth', require('./routes/auth'))
+app.use('/', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
   console.log(`INotebook app listening on port ${port}`);
