ce604dc414c3b6a114e4b90c0fa822dd773b92bd
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-06-28 14:24:33 +0500

    ...

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 app.use('/', (req,res)=>{
-  res.json({message:req.body.message})
+  res.send("hello")
 });
 
 app.listen(port, () => {
