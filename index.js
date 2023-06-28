dfd02fd8d55d72c16224a755c5f60ab5d3fd50c4
Daniyal-Lodhi <98033919+Daniyal-Lodhi@users.noreply.github.com>
2023-06-28 23:41:28 +0500

    ..

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(express.json())
 /*(/api/auth)-->idhr middleware function exceute hoga ,(./ routes/auth)--> idhr middleware function hy */ 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
-app.use('/z', (req,res)=>{
-  res.send("hello")
-});
+// app.use('/', (req,res)=>{
+//   res.send("hello")
+// });
 
 app.listen(port, () => {
   console.log(`INotebook app listening on port ${port}`);
--- routes/hej.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/printhej',(req,res)=>{
-    res.send('hej');
-})
- 
-module.exports = router 
